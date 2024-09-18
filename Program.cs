068d4e5e1443de530d1ba662b9582db7af42c28b
august0813 <hudaadila31@gmail.com>
2024-09-18 09:44:30 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //creating an object (variable of a cetain class type)
+        Transaction transaction = new Transaction();
+
+        transaction.date = new DateTime(2008, 6, 1);
+        transaction.val = 10.5f;
+
+        Console.WriteLine("transaction value is " + transaction.val + " at time " + transaction.date.ToString());
+    }
+
+}
---
+++ Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+  public class Transaction
+    {
+    public float val;
+    public DateTime date;
+    }
